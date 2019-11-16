cc7b3dd56fe4b8c5cb1d01a8d94f2d24430d4318
Omri Lavi <omril321@gmail.com>
2019-11-16 14:19:35 +0200

    Added header, improved layout

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './Header.scss';
+
+
+const Header = ({}) => {
+    return (
+        <header className="header">
+            <div className="header_content-container">
+                <div>Image here</div>
+                <div>Links here</div>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,20 +2,22 @@ import * as React from 'react';
 import './Homepage.scss';
 import PostLink from "./PostLink/PostLink";
 import AboutMe from "./AboutMe/AboutMe";
+import Header from "../Header/Header";
 
 
 const Homepage = ({postsMetadata}) => {
     return (
-        <div className="homepage">
-            <div className="homepage_padding"/>
-            <div className="homepage_content">
-                <AboutMe/>
-                <div className="homepage_post-links-container">
-                    {postsMetadata.map(metadata => <PostLink key={metadata.url} {...metadata}/>)}
+        <>
+            <Header/>
+            <div className="homepage">
+                <div className="homepage_content">
+                    <AboutMe/>
+                    <div className="homepage_post-links-container">
+                        {postsMetadata.map(metadata => <PostLink key={metadata.url} {...metadata}/>)}
+                    </div>
                 </div>
             </div>
-            <div className="homepage_padding"/>
-        </div>
+        </>
     )
 };
 
