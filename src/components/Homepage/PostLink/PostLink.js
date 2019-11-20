2920159070822b57e0e35f6006f3d7f76b8c5ba8
Omri Lavi <omril321@gmail.com>
2019-11-20 08:18:37 +0200

    improved layout

--- src/components/Homepage/PostLink/PostLink.js
+++ src/components/Homepage/PostLink/PostLink.js
@@ -6,14 +6,18 @@ const PostLink = ({title, icon, description, image, url, type, provider, flags})
         <a className="post-link" href={url} target={"_blank"}>
             <div className="post-link_image-container">
                 <div className="post-link_image" style={{backgroundImage: `url(${image})`}}/>
-                <div className="post-link_icon" style={{backgroundImage: `url(${icon})`}}/>
             </div>
             <div>
                 <div className="post-link_title">{title}</div>
                 <div className="post-link_description">{description}</div>
             </div>
+            <div className="post-link_additional-info">
+                <div className="post-link_icon" style={{backgroundImage: `url(${icon})`}}/>
+                <div>Reading time</div>
+                <div>{type}</div>
+            </div>
         </a>
     );
-}
+};
 
 export default PostLink;
