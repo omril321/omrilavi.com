39c1197391faa08b3f16933354179e6d9d3f8237
Omri Lavi <omril321@gmail.com>
2019-11-30 16:41:28 +0200

    using a variable for media queries

--- src/components/Homepage/PostLink/PostLink.js
+++ src/components/Homepage/PostLink/PostLink.js
@@ -7,10 +7,10 @@ const PostLink = ({title, icon, description, image, url, type, provider, flags})
             <div className="post-link_image-container">
                 <div className="post-link_image" style={{backgroundImage: `url(${image})`}}/>
             </div>
-            <div>
+            <article>
                 <div className="post-link_title">{title}</div>
                 <div className="post-link_description">{description}</div>
-            </div>
+            </article>
             <div className="post-link_additional-info">
                 <div className="post-link_icon" style={{backgroundImage: `url(${icon})`}}/>
                 {/*<div>Reading time</div>*/}
