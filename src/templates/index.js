5804fea3fcba996d8f06d13002131ab1372357c1
Omri Lavi <omril321@gmail.com>
2019-12-03 10:25:19 +0200

    fixed the typo for real this time

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Homepage from "../components/Homepage/homepage";
+import Homepage from "../components/Homepage/Homepage";
 
 export default ({pageContext: {allPostsMetadata}}) => {
     return (
