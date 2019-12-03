c029b08a59bebdb461f296efa17991f11750cf3a
Omri Lavi <omril321@gmail.com>
2019-12-03 18:39:49 +0200

    improved favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         background_color: `${primaryColor}`,
         theme_color: `${primaryColor}`,
         display: `minimal-ui`,
-        icon: `src/images/me.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
