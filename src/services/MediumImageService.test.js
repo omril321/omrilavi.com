be2e5298fabed78e0db6e14f520c3a38b9a4c1db
Omri Lavi <omril321@gmail.com>
2019-12-08 07:58:35 +0200

    test commit for failed tests

--- src/services/MediumImageService.test.js
+++ src/services/MediumImageService.test.js
@@ -26,7 +26,7 @@ describe('MediumImageService', () => {
 
             const result = attemptSettingMediumImageUrlByWidth(input, 123);
 
-            expect(result).toBe(expected);
+            expect(result).toBe("");
         });
 
         it('should return an altered URL with given width when input is a Medium image which has additional path info', () => {
