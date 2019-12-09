b441543c57f2606eaa9ed1c49116e4dc53644557
Omri Lavi <omril321@gmail.com>
2019-12-09 08:16:33 +0200

    added cypress tests

---
+++ cypress/integration/homepage.test.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const websiteIndexHtmlFile = path.resolve(__dirname, "..", "..", "public", "index.html");
+const postsConfig = require('../../src/config/posts');
+
+describe("Homepage", () => {
+    beforeEach(() => {
+        cy.visit("index.html");
+        cy.wait(500);
+    });
+
+    it('should have the same number of posts like the configuration', async () => {
+        cy.get(".page-layout")
+            .find("a.post-link")
+            .should('have.lengthOf', postsConfig.posts.length);
+    });
+});
