77a87bfc3c7fe772fa488fc441648a82fb3bd2e7
Omri Lavi <omri@testim.io>
2020-09-28 16:06:16 +0300

    added publish date

--- src/ssr/postMetadataFetcher.js
+++ src/ssr/postMetadataFetcher.js
@@ -1,10 +1,10 @@
-const {getMetadata} = require('page-metadata-parser');
+const { getMetadata, metadataRuleSets } = require('page-metadata-parser');
 const domino = require('domino');
 const fetch = require('node-fetch');
 
-const expandPostMetadata = async ({overrides = {}, flags = [], url}) => {
-    const {title, icon, description, image, provider, type, ...rest} = await fetchMetadataForUrl(url);
-    const result = {title, icon, description, image, url, provider, type, flags, ...overrides};
+const expandPostMetadata = async ({ overrides = {}, flags = [], url }) => {
+    const { title, icon, description, image, provider, type, publishDate, ...rest } = await fetchMetadataForUrl(url);
+    const result = { title, icon, description, image, url, provider, type, flags, publishDate, ...overrides };
 
     throwIfAnyMetadataFieldIsMissing(result);
 
@@ -15,11 +15,25 @@ const fetchMetadataForUrl = async (url) => {
     const response = await fetch(url);
     const html = await response.text();
     const doc = domino.createWindow(html).document;
-    return await getMetadata(doc, url);
+    const rules = {
+        ...metadataRuleSets,
+        ...{
+            publishDate: {
+                rules: [
+                    ['meta[property="article:published_time"]', element => element.getAttribute('content')],
+                    ['meta[itemprop="datePublished"]', element => element.getAttribute('content')],
+                ],
+                processors: [
+                    (dateOrTime, context) => new Date(dateOrTime)
+                ],
+            },
+        }
+    }
+    return getMetadata(doc, url, rules);
 };
 
 function throwIfAnyMetadataFieldIsMissing(obj) {
-    const {url} = obj;
+    const { url } = obj;
     for (const [key, value] of Object.entries(obj)) {
         throwIfNotExists(url, key, value);
     }
