2d2902182eea2becc27bf05b16a226b6853d440f
Omri Lavi <omri@testim.io>
2021-08-20 21:25:18 +0300

    start using tailwind

--- src/components/Homepage/AboutMe/AboutMe.js
+++ src/components/Homepage/AboutMe/AboutMe.js
@@ -13,10 +13,10 @@ const AboutMe = () => {
             <div className="about-me_image-wrapper">
                 <MeImage/>
             </div>
-            <div className="about-me_description-wrapper">
-                <h2>Hi, I'm Omri Lavi <span role="img" aria-label="hello">👋</span></h2>
+            <div className="flex flex-col justify-between mt-7 ml-0 md:mt-0 md:ml-7 items-center text-primary md:items-start">
+                <h2 className="filter drop-shadow-md">Hi, I'm Omri Lavi <span role="img" aria-label="hello">👋</span></h2>
                 <PersonalLinks/>
-                <p className="about-me_description-text">
+                <p className="text-primary-dark whitespace-pre-wrap text-center md:text-left text-lg">
                     {description}
                 </p>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const color = require('color');
+
+const primaryColor = '#2f98b0'; //using split-complementary colors
+const secondaryColor = '#ab832e';
+const tertiaryColor = '#ab2e55';
+
+const darken = _color => color(_color).darken(35).toString();
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: primaryColor,
+        secondary: secondaryColor,
+        tertiary: tertiaryColor,
+
+        'primary-dark': darken(primaryColor),
+        'secondary-dark': darken(secondaryColor),
+        'tertiary-dark': darken(tertiaryColor),
+      },
+    },
   },
   variants: {
     extend: {},
