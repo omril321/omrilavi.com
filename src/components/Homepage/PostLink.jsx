9b54a100cff47dd5d4a752d2f6f3a9ed08e35e5f
Omri Lavi <omri@testim.io>
2021-08-30 18:40:49 +0300

    small improvements

--- src/components/Homepage/PostLink.jsx
+++ src/components/Homepage/PostLink.jsx
@@ -8,7 +8,7 @@ const PostLink = ({ title, icon, description, image, url, type, provider, flags,
     const dateStr = new Date(publishDate).toLocaleDateString('EN', { year: 'numeric', month: 'long', day: 'numeric' });
 
     return (
-        <a className="h-64 rounded-xl transition-all group bg-normal hover:bg-zoomed-in bg-center text-center overflow-hidden duration-700 mb-8 shadow-sharp" data-test-post-link
+        <a className="h-64 rounded-xl transition-all group hover:bg-zoomed-in bg-center text-center overflow-hidden duration-700 mb-8 shadow-sharp bg-no-repeat bg-cover" data-test-post-link
             style={{ backgroundImage: `url(${adaptedImageUrl})` }} href={url}>
             <div className="h-full bg-primary-xlight bg-opacity-90 flex flex-col shadow-strong-inner">
                 <div className="h-64 flex group-hover:h-0 transition-all flex-shrink-0 items-center overflow-hidden relative">
@@ -18,7 +18,7 @@ const PostLink = ({ title, icon, description, image, url, type, provider, flags,
 
                 <div className="text-lg font-semibold h-full transition flex items-center bg-opacity-30 bg-secondary-light relative overflow-hidden">
                     <span className="px-10 flex-grow">{description}</span>
-                    <div className="absolute bottom-4 right-4 w-6 h-6 self-end bg-cover shadow-md" style={{ backgroundImage: `url(${icon})` }} />
+                    <div className="absolute bottom-4 right-4 w-6 h-6 self-end bg-cover filter drop-shadow-md" style={{ backgroundImage: `url(${icon})` }} />
                 </div>
             </div>
         </a>
