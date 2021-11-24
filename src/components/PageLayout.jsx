857f5d0d739b59bc634d3713c91a153bbc91de7a
Omri Lavi <omri@testim.io>
2021-11-24 09:39:13 +0200

    fix page layout

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import "typeface-open-sans";
 import Seo from "./seo";
-// import SocialButtons from "./common/SocialButtons/SocialButtons";
 
 const PageLayout = ({children}) => (
-    <div className="bg-bodyColor flex flex-col lg:flex-row-reverse fixed inset-0" data-test-page-layout>
+    <div className="bg-bodyColor flex flex-col" data-test-page-layout>
         <Seo/>
         <div className="mx-auto py-11 overflow-scroll">
             {children}
         </div>
-        {/* <div className="bg-gray-900 lg:bg-opacity-0 flex lg:fixed -inset-y-0 left-0 items-center">
-            <SocialButtons/>
-        </div> */}
     </div>
 );
 
