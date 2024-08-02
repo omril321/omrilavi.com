c85e4520c699705b8f70c755cae9c523efa32eee
omril321 <omril321@gmail.com>
2024-08-02 22:12:13 +0300

    Updated personal images

--- server/@qwik-city-static-paths.js
+++ server/@qwik-city-static-paths.js
@@ -1,4 +1,4 @@
-const staticPaths = new Set(["/.DS_Store","/favicon-120x120.png","/favicon-144x144.png","/favicon-16x16.png","/favicon-196x196.png","/favicon-32x32.png","/favicon.png","/favicon.svg","/manifest.json","/me.png","/me_large.png","/q-manifest.json","/robots.txt","/service-worker.js"]);
+const staticPaths = new Set(["/.DS_Store","/_headers","/favicon-120x120.png","/favicon-144x144.png","/favicon-16x16.png","/favicon-196x196.png","/favicon-32x32.png","/favicon.png","/favicon.svg","/manifest.json","/me.png","/me_large.png","/q-manifest.json","/robots.txt","/service-worker.js"]);
 function isStaticPath(method, url) {
   if (method.toUpperCase() !== 'GET') {
     return false;
