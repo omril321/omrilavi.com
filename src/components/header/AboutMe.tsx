a4e1d0edbc9873029e72abfdb2c2ebce44c602a1
omril321 <omril321@gmail.com>
2024-09-29 09:15:15 +0300

    Added more and less buttons, migrate to vitest

--- src/components/header/AboutMe.tsx
+++ src/components/header/AboutMe.tsx
@@ -1,12 +1,19 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import SocialButtons from "../socialButtons";
 import MeImage from "./MeImage";
-import { ABOUT_ME_DESCRIPTION } from "../../config/constants";
+import { ABOUT_ME } from "../../config/constants";
 
 export default component$(() => {
+  const description = useSignal(ABOUT_ME.short);
+
+  const nextMore = description.value === ABOUT_ME.short ? ABOUT_ME.medium : ABOUT_ME.long;
+  const hasMore = nextMore !== description.value;
+  const nextLess = description.value === ABOUT_ME.long ? ABOUT_ME.medium : ABOUT_ME.short;
+  const hasLess = nextLess !== description.value;
+
   return (
-    <div class="flex m-auto items-center justify-center">
-      <div class="flex-shrink-0 rounded-full hidden sm:block w-32 overflow-hidden filter drop-shadow-2xl">
+    <div class="flex m-auto items-start justify-center">
+      <div class="flex-shrink-0 flex-start rounded-full hidden sm:block w-32 overflow-hidden filter drop-shadow-2xl">
         <MeImage />
       </div>
       <div class="sm:ml-8">
@@ -17,8 +24,30 @@ export default component$(() => {
             Building Tools for Developers
           </span>
         </h2>
-        <p class="text-secondary-light whitespace-pre-wrap text-lg font-semibold">{ABOUT_ME_DESCRIPTION}</p>
-        <div class="flex flex-1 justify-center mb-4 sm:justify-start sm:mb-0">
+        <p aria-label="description" class="text-secondary-light whitespace-pre-wrap text-lg font-semibold">
+          {description}
+        </p>
+        <span class="flex gap-x-3">
+          {hasLess && (
+            <button
+              class="text-secondary-light text-sm bg-primary-dark px-2 py-1 rounded whitespace-pre-wrap hover:bg-primary transition-colors"
+              aria-label="Time to Breathe"
+              onClick$={() => (description.value = nextLess)}
+            >
+              😮‍💨{"   "}Time to Breathe
+            </button>
+          )}
+          {hasMore && (
+            <button
+              class="text-secondary-light text-sm bg-primary-dark px-2 py-1 rounded whitespace-pre-wrap hover:bg-primary transition-colors"
+              aria-label="Go Deeper"
+              onClick$={() => (description.value = nextMore)}
+            >
+              🤿{"   "}Go Deeper
+            </button>
+          )}
+        </span>
+        <div class="flex flex-1 justify-center mt-3 mb-4 sm:justify-start sm:mb-0">
           <SocialButtons />
         </div>
       </div>
