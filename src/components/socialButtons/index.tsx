8991eb98c35727e254b3617da73d31b89069d032
omril321 <omril321@gmail.com>
2024-11-22 16:47:52 +0200

    add aria labels to links

--- src/components/socialButtons/index.tsx
+++ src/components/socialButtons/index.tsx
@@ -2,12 +2,23 @@ import { component$ } from "@builder.io/qwik";
 import type { JSXChildren } from "@builder.io/qwik";
 import { GitHubIcon, LinkedInIcon, MediumIcon, TwitterIcon, DevToIcon } from "./Icons";
 
-const SBWrapper = ({ url, iconSvg, clazz }: { url: string; iconSvg: () => JSXChildren; clazz: string }) => (
+const SBWrapper = ({
+  url,
+  iconSvg,
+  clazz,
+  label,
+}: {
+  url: string;
+  iconSvg: () => JSXChildren;
+  clazz: string;
+  label: string;
+}) => (
   <a
     class={`${clazz} w-5 h-4 p-1 m-1 box-content transition-all text-white hover:text-primary`}
     href={url}
     target="_blank"
     rel="noopener noreferrer"
+    aria-label={label}
   >
     <div class="fill-current">{iconSvg()}</div>
   </a>
@@ -15,11 +26,21 @@ const SBWrapper = ({ url, iconSvg, clazz }: { url: string; iconSvg: () => JSXChi
 
 const SocialButtons = component$(() => (
   <>
-    <SBWrapper url="https://github.com/omril321" iconSvg={GitHubIcon} clazz="border-github" />
-    <SBWrapper url="https://linkedin.com/in/omri-lavi" iconSvg={LinkedInIcon} clazz="border-linkedin" />
-    <SBWrapper url="https://dev.to/omril321" iconSvg={DevToIcon} clazz="border-devto" />
-    <SBWrapper url="https://medium.com/@omril321" iconSvg={MediumIcon} clazz="border-medium" />
-    <SBWrapper url="https://twitter.com/omril321" iconSvg={TwitterIcon} clazz="border-twitter" />
+    <SBWrapper
+      url="https://linkedin.com/in/omri-lavi"
+      iconSvg={LinkedInIcon}
+      clazz="border-linkedin"
+      label={"Linkedin account"}
+    />
+    <SBWrapper url="https://github.com/omril321" iconSvg={GitHubIcon} clazz="border-github" label={"GitHub account"} />
+    <SBWrapper url="https://dev.to/omril321" iconSvg={DevToIcon} clazz="border-devto" label={"Dev.To account"} />
+    <SBWrapper url="https://medium.com/@omril321" iconSvg={MediumIcon} clazz="border-medium" label={"Medium account"} />
+    <SBWrapper
+      url="https://twitter.com/omril321"
+      iconSvg={TwitterIcon}
+      clazz="border-twitter"
+      label={"Twitter account"}
+    />
   </>
 ));
 
