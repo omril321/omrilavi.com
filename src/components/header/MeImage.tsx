ae267e60125fbbb7290edab6f1cd53fb67c053d7
omril321 <omril321@gmail.com>
2024-11-22 16:49:56 +0200

    add alt

--- src/components/header/MeImage.tsx
+++ src/components/header/MeImage.tsx
@@ -4,7 +4,7 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <div style={{ height: "144px", width: "144px" }}>
-      <ImgMe />
+      <ImgMe alt="This is Omri Lavi" />
     </div>
   );
 });
