930ea7f163fda158c0958358d2a6177f6bfd1c9a
Omri Lavi <omril321@gmail.com>
2025-08-17 10:02:48 +0300

    Remove usage of vite-tsconfig-paths due to type errors, using manual setup instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@qwik.dev/core/optimizer";
 import { qwikRouter } from "@qwik.dev/router/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { partytownVite } from "@builder.io/partytown/utils";
 import { join } from "path";
 
@@ -10,9 +9,13 @@ export default defineConfig(() => {
     plugins: [
       qwikRouter(),
       qwikVite(),
-      tsconfigPaths(),
       partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
     ],
+    resolve: {
+      alias: {
+        "~": join(__dirname, "src"),
+      },
+    },
     ssr: {
       noExternal: ["https", "crypto", "path"],
     },
